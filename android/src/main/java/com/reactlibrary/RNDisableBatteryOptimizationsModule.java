96d89f9e14444915612d93bce59a74f72b35a58f
Marisa Gomez <marisa@cuttlesoft.com>
2020-07-08 15:32:00 -0400

    ✨(Module) Utilize `startActivityForResult` in order to get result back from dialog
    --

--- android/src/main/java/com/reactlibrary/RNDisableBatteryOptimizationsModule.java
+++ android/src/main/java/com/reactlibrary/RNDisableBatteryOptimizationsModule.java
@@ -12,13 +12,16 @@ import android.net.Uri;
 import android.os.Build;
 
 
-public class RNDisableBatteryOptimizationsModule extends ReactContextBaseJavaModule {
+public class RNDisableBatteryOptimizationsModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+  static final int REQUEST_REMOVAL_BATTERY_OPTIMIZATION = 1;
 
   private final ReactApplicationContext reactContext;
+  private Promise promise;
 
   public RNDisableBatteryOptimizationsModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    this.reactContext.addActivityEventListener(this);
   }
 
   @ReactMethod
@@ -29,7 +32,7 @@ public class RNDisableBatteryOptimizationsModule extends ReactContextBaseJavaMod
 			intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
 			intent.setData(Uri.parse("package:" + packageName));
 			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			reactContext.startActivity(intent);
+			reactContext.startActivityForResult(intent);
     
 	  }
 
@@ -48,24 +51,30 @@ public class RNDisableBatteryOptimizationsModule extends ReactContextBaseJavaMod
     promise.resolve(false);
   }
 
-
-
-
   @ReactMethod
-  public void enableBackgroundServicesDialogue() {
+  public void enableBackgroundServicesDialogue(Promise promise) {
+    this.promise = promise;
+
     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
       Intent myIntent = new Intent();
       String packageName =  reactContext.getPackageName();
       PowerManager pm = (PowerManager) reactContext.getSystemService(reactContext.POWER_SERVICE);
-      if (pm.isIgnoringBatteryOptimizations(packageName))
+      
+      if (pm.isIgnoringBatteryOptimizations(packageName)) {
         myIntent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
-      else {
+      } else {
         myIntent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
         myIntent.setData(Uri.parse("package:" + packageName));
       }
-	  myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      reactContext.startActivity(myIntent);
+      
+	    myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      reactContext.startActivityForResult(myIntent, REQUEST_REMOVAL_BATTERY_OPTIMIZATION, null);
     }
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    this.promise.resolve(data.getDataString());
   }
 
   @Override
